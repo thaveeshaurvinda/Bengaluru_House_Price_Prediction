38c0dd2b416a007f0226e8741990ec2baeff4a04
Emaduwage Thaveesha Urvinda <thaveeshaurvinda2007@gmail.com>
2026-02-13 19:07:25 +0530

    Implement home price prediction API and enhance frontend UI
    - Added Flask routes for fetching location names and predicting home prices.
    - Integrated utility functions for model predictions and location retrieval.
    - Created a responsive HTML interface for user input with CSS styling.
    - Implemented JavaScript functions to handle user interactions and API requests.

---
+++ client/app.js
@@ -0,0 +1,72 @@
+function getBathValue() {
+  var uiBathrooms = document.getElementsByName("uiBathrooms");
+  for (var i = 0; i < uiBathrooms.length; i++) {
+      if (uiBathrooms[i].checked) {
+          return parseInt(uiBathrooms[i].value);
+      }
+  }
+  return -1;
+}
+
+function getBHKValue() {
+  var uiBHK = document.getElementsByName("uiBHK");
+  for (var i = 0; i < uiBHK.length; i++) {
+      if (uiBHK[i].checked) {
+          return parseInt(uiBHK[i].value);
+      }
+  }
+  return -1;
+}
+
+
+function onClickedEstimatePrice() {
+  console.log("Estimate price button clicked");
+
+  var sqft = document.getElementById("uiSqft");
+  var bhk = getBHKValue();
+  var bathrooms = getBathValue();
+  var location = document.getElementById("uiLocations");
+  var estPrice = document.getElementById("uiEstimatedPrice");
+
+  var url = "http://127.0.0.1:5000/predict_home_price";
+
+  $.post(url, {
+      total_sqft: parseFloat(sqft.value),
+      bhk: bhk,
+      bath: bathrooms,
+      location: location.value
+  }, function(data, status) {
+
+      console.log(data.estimated_price);
+
+      estPrice.innerHTML = data.estimated_price + " Lakh";
+
+      // Trigger animation
+      estPrice.classList.remove("show");
+      void estPrice.offsetWidth;
+      estPrice.classList.add("show");
+
+      console.log(status);
+  });
+}
+
+
+function onPageLoad() {
+  console.log( "document loaded" );
+  var url = "http://127.0.0.1:5000/get_location_names";
+  $.get(url,function(data, status) {
+      console.log("got response for get_location_names request");
+      if(data) {
+          var locations = data.locations;
+          var uiLocations = document.getElementById("uiLocations");
+          $('#uiLocations').empty();
+          for(var i in locations) {
+              var opt = new Option(locations[i]);
+              $('#uiLocations').append(opt);
+          }
+      }
+  });
+}
+
+window.onload = onPageLoad;
+
--- server/util.py
+++ server/util.py
@@ -1,13 +1,12 @@
-import json
 import pickle
+import json
 import numpy as np
 
-# Global variables (non public use variables)
 __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location, sqft, bhk, bath):
+def get_estimated_price(location,sqft,bhk,bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -17,37 +16,37 @@ def get_estimated_price(location, sqft, bhk, bath):
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index >= 0:
+    if loc_index>=0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0], 2)
+    return round(__model.predict([x])[0],2)
 
 
-# Routine 1
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global  __data_columns
+    global __locations
+
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+
+    global __model
+    if __model is None:
+        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+            __model = pickle.load(f)
+    print("loading saved artifacts...done")
+
 def get_location_names():
     return __locations
 
-def load_saved_artifacts():
-    print("Loading saved artifacts")
-    global __data_columns
-    global __locations
+def get_data_columns():
+    return __data_columns
 
-    # Loading data columns and locations
-    with open("./artifacts/columns.json", "r") as f:
-        __data_columns = json.load(f)["data_columns"]
-        __locations = __data_columns[3:]
-
-    # Loading the model
-    global __model
-    with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
-        __model = pickle.load(f)
-    print("Successfully loaded artifacts")
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2))
-    print(get_estimated_price('Ejipura', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
